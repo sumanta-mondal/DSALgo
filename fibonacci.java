0c5781ae0921f718e76aea64642c9f4c2e3beadd
sumanta-mondal <sumanta.ju09@gmail.com>
2022-06-09 18:14:23 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n==0){
+            System.out.print(1);
+        }else if(n==1){
+            System.out.print(1+" ");
+            System.out.print(1);
+        }else{
+            System.out.print(1+" ");
+            System.out.print(1+" ");
+            int a=1;
+            int b=1;
+
+            for(int i=2;i<=n;i++){
+                int c=a+b;
+                System.out.print(c+" ");
+                a=b;
+                b=c;
+            }
+        }
+    }
+}
