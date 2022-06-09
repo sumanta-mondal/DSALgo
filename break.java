aea6ce8c122a9a304e26d9926d3a3f7286126b39
sumanta-mondal <sumanta.ju09@gmail.com>
2022-06-09 09:45:36 +0530

    loop with brek in conditon

---
+++ break.java
@@ -0,0 +1,22 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+
+public class Main
+{ 
+    public static void main(String[] args)throws IOException 
+    { 
+      for(int i=0; i<2; i++)
+      {
+         for(int j=0; j<2; j++)
+         {
+             if(j==1)
+             break;
+             
+             System.out.println(j);
+         }
+      }
+
+    } 
+} 
